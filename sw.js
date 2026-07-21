d8f1f0fa13f5918bd42b13691daa0d7edc038066
kfca123 <frezmo.tshirt@gmail.com>
2026-07-21 08:56:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// bump this version string whenever any file changes, or users keep the old cached version
+const CACHE = 'nonsense-v3';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }))
+  );
+});
